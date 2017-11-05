acc12b5181a89b8ce5bd6f025484d3c288f40e68
lti <landry.tiehi@gmail.com>
2017-11-05 17:05:06 +0100

    build script added

---
+++ bin/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+
+artefact_name=designer-webapp.tar.gz
+
+# Build with node
+node make
+
+# package
+tar cvfz $artefact_name ../html ../library ../lib ../css
+
+# Clean and/or create target folder
+rm -rf ../target
+mkdir ../target
+
+# copy to target folder
+cp $artefact_name ../target/
+
+# Remove archive from bin directory
+rm ./${artefact_name}
