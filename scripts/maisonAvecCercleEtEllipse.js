ab974f1511572adeac0ec23d9efa2d9b1d1cbac0
lti <landry.tiehi@gmail.com>
2017-11-05 16:46:52 +0100

    New structure with library

---
+++ scripts/maisonAvecCercleEtEllipse.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// Creation du builder
+	var designer = esiee_components.get_designer();         
+
+	// Propriétés du composant à dessiner
+	designer.canvas_container_id = "my_canvas_container_id_2";
+	designer.nom = "maisonAvecCercleEtEllipse";
+
+	// Creation du graphique apres chargement de la page
+	designer.drawComponents();
+}, false);
