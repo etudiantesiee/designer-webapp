e88f3bbc8b5e43440b1b2b508b99544206a4c7cc
lti <landry.tiehi@gmail.com>
2017-12-06 01:01:06 +0100

    variabilisation des conf artifactory et web app

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,5 +5,15 @@
 # Install before deploying
 ./install.sh
 
+# Artifactory conf
+artifactory_host=localhost
+artifactory_port=8081
+artifactory_user=admin
+artifactory_pwd=password
+
+# Web app conf
+web_app_name=designer-webapp
+web_app_version=0.0.1-SNAPSHOT
+
 # deploy to artifactory
-curl -u admin:password -X PUT "http://localhost:8081/artifactory/libs-snapshot-local/fr/esiee/pic/designer-webapp/0.0.1-SNAPSHOT/designer-wbapp.tar.gz" -T ../target/designer-webapp.tar.gz
+curl -u ${artifactory_user}:${artifactory_pwd} -X PUT "http://${artifactory_host}:${artifactory_port}/artifactory/libs-snapshot-local/fr/esiee/pic/designer-webapp/${web_app_version}/${web_app_name}.tar.gz" -T ../target/${web_app_name}.tar.gz
